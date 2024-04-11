13c7e0fb842b0d86adf62da220d49256ebc493ab
toppu <dentsuboy.bm@gmail.com>
2024-04-11 19:34:21 +0200

    init setup

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Box, Rating } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          Edit and save to reload.
+          <Rating value={3} />
+          <Button />
+        </h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
